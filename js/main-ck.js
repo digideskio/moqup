14fd1d28a5a104dca1d7cf906ec3be241506fd55
Ben Davis <ben.davis.ok@gmail.com>
2013-10-05 21:24:52 -0400

    js for interviewers list animations

--- js/main-ck.js
+++ js/main-ck.js
@@ -1,1 +1,1 @@
-$(function(){$("a.request-interview").click(function(){$(this).hide().parent().find("p.request-sent").show()})});
+$(function(){$("a.request-interview").click(function(){$(this).hide().parent().find("p.request-sent").show();$(this).parent().parent().delay(5e3).animate({height:"toggle",opacity:"toggle",padding:"toggle",margin:"toggle"},2500)})});
--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(function(){
 	$('a.request-interview').click(function(){
 		$(this).hide().parent().find('p.request-sent').show();
+		$(this).parent().parent().delay(5000).animate({ height:"toggle", opacity:"toggle", padding:"toggle", margin:"toggle" }, 2500);
 	});
 });
